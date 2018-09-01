2f70a0936effe1e7c8beaf39b276d51a44229efb
Ger Hobbelt <ger@hobbelt.com>
2018-09-01 20:58:59 +0200

    - tweak test rig to regenerate SOLLWERT files (`expected.js`) when these are missing. - + Dirty hack to tweak the sources to ensure all platforms (Windows vs. UNIX) produce the same output: tweak the forward-slash to backslash for all `filepath` entries in there!

--- test/test.js
+++ test/test.js
@@ -18,11 +18,24 @@ function testTransform (fixtureName, extraOptions) {
             ]
         }, extraOptions));
         var actual = result.code + '\n';
-        var expected = fs.readFileSync(expectedFilepath, 'utf8');
-        if (actual != expected) {
-            fs.writeFileSync(actualFilepath, actual);
+
+        // dirty hack to tweak the sources to ensure all platforms (Windows vs. UNIX) produce the same output:
+        // tweak the forward-slash to backslash for all `filepath` entries in there:
+        actual = actual.replace(/filepath:.*$/gm, function (line) {
+            return line.replace(/[\\][\\]/g, '/');
+        });
+
+        if (fs.existsSync(expectedFilepath)) {
+            var expected = fs.readFileSync(expectedFilepath, 'utf8');
+            if (actual !== expected) {
+                fs.writeFileSync(actualFilepath, actual, 'utf8');
+            }
+            assert.equal(actual, expected);
+        } else {
+            console.warn("          Regenerating test SOLLWERT for " + fixtureName + " ...");
+            assert(true); // shut up test rig: one (fake) test done at least!
+            fs.writeFileSync(expectedFilepath, actual, 'utf8');
         }
-        assert.equal(actual, expected);
     });
 }
 
